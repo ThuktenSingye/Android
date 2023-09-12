c77ab7f0dc800954360ce736425a7515b568ce3b
ThuktenSingye <thuktensingye2163@gmail.com>
2023-09-12 15:04:59 +0600

    Async Task Loader # API fetch-Internet-con

---
+++ AndroidTutorial/AsyncTaskLoaderDemo/app/src/main/java/com/example/asynctaskloaderdemo/BookLoader.java
@@ -0,0 +1,27 @@
+package com.example.asynctaskloaderdemo;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader {
+    private String mQueryString;
+    public BookLoader(@NonNull Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public Object loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+}
